d7ce4a589b46aecd9abf2f284d3e02eb89c5ebbc
shashank231 <65719041+shashank231@users.noreply.github.com>
2024-09-09 19:59:43 +0530

    a1

--- 09. Dynamic Routes and Advanced Models/app.js
+++ 09. Dynamic Routes and Advanced Models/app.js
@@ -10,15 +10,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.set('view engine', 'ejs');
-
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorsController.get404);
 
 app.listen(3000);
