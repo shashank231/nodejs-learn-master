3f06faa36087b91c0ef1543f7640db7798e9e2a5
shashank231 <65719041+shashank231@users.noreply.github.com>
2024-09-13 21:57:23 +0530

    Explaining sequelize models

--- 11. Understading Sequelize/app.js
+++ 11. Understading Sequelize/app.js
@@ -36,19 +36,35 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorsController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+// A Product belongs to a User, meaning a product has a foreign key (userId) pointing to the User model.
+// constraints: true enforces referential integrity, meaning Sequelize will ensure that the userId column references a valid User.
+// onDelete: 'CASCADE' ensures that when a User is deleted, all associated Products will be deleted as well.
 User.hasMany(Product);
-User.hasOne(Cart);
+// A User can have many Products, establishing a one-to-many relationship where the User is the parent and the Product is the child. This complements the Product.belongsTo(User) relationship.
+
 Cart.belongsTo(User);
+// A Cart belongs to a User, meaning the Cart has a foreign key (userId) pointing to the User. This complements the User.hasOne(Cart) relationship.
+User.hasOne(Cart);
+// A User can have one Cart. This defines a one-to-one relationship between User and Cart. The Cart table will have a foreign key userId to reference the User.
+
 Product.belongsToMany(Cart, { through: CartItem });
+// A Product can belong to many Carts, and vice versa. This is a many-to-many relationship, where the association is managed through an intermediate model (CartItem). The CartItem model will store cartId and productId as foreign keys.
 Cart.belongsToMany(Product, { through: CartItem });
+// A Cart can contain many Products, establishing the inverse of the previous relationship. Again, this relationship is mediated by the CartItem model.
+
 Order.belongsTo(User);
+// An Order belongs to a User, meaning the Order has a foreign key (userId) referencing the User model.
 User.hasMany(Order);
+// A User can have many Orders, establishing a one-to-many relationship between User and Order. This complements the Order.belongsTo(User) relationship.
+
 Order.belongsToMany(Product, { through: OrderItem });
+// An Order can contain many Products, and vice versa. This is a many-to-many relationship managed through the OrderItem model, which will store orderId and productId.
 Product.belongsToMany(Order, { through: OrderItem });
+// A Product can be part of many Orders, establishing the inverse of the previous relationship, mediated by the OrderItem model.
+
 
 sequelize
     .sync()
@@ -78,3 +94,4 @@ sequelize
     .catch(err => {
         if(err) console.log(err)
     });
+
